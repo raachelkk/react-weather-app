c367983b59a75d37666dcba40d8b4783732c7d29
raachelkk <116052393+raachelkk@users.noreply.github.com>
2023-02-20 14:10:25 -0800

    Added link to Github and credit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This page was coded using React by Rachel Komori and is{" "}
+        <a
+          href="https://github.com/raachelkk/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
