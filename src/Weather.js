0684b6fbb315841e8b44032c39a1986cf87a04b4
raachelkk <116052393+raachelkk@users.noreply.github.com>
2023-02-20 15:01:43 -0800

    Added search form

--- src/Weather.js
+++ src/Weather.js
@@ -5,6 +5,20 @@ export default function Weather() {
   return (
     <div className="Weather">
       <div className="container">
+        <form>
+          <div className="row">
+            <div className="col-8">
+              <input
+                className="form-control"
+                type="search"
+                placeholder="Enter a city..."
+              />
+            </div>
+            <div className="col-4">
+              <input className="btn btn-light" type="submit" value="Search" />
+            </div>
+          </div>
+        </form>
         <h1>Victoria</h1>
         <ul>
           <li>Monday 2:00pm</li>
