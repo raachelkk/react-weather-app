f54d032c038bddafe00b7f2bd81e49792c4f581c
raachelkk <116052393+raachelkk@users.noreply.github.com>
2023-03-02 19:51:04 -0800

    Set forecast to update with city search

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,11 +1,15 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Forecast.css";
 import ForecastDay from "./ForecastDay";
 
 export default function Forecast(props) {
   const [loaded, setLoaded] = useState(false);
   const [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setLoaded(true);
